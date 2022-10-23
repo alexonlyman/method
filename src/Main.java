4f4cf7cd9e051042288a91cd1479b96fe92663ab
Алекс <chu@mail.ru>
2022-10-23 12:54:31 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+    public static int checkYear (int year) {
+        int years = year % 4;
+        return years;
+
+    }
+
     public static void main(String[] args) {
+        int year = 2022;
+        if (checkYear(year) == 0)
+            System.out.println(" високосный ");
+        else System.out.println(" не високосный ");
+        int specialYear = checkYear(year);
 
     }
 }
