d0421e9700ee61ece76fd010c939d8bfd3644a4f
Алекс <chu@mail.ru>
2022-10-27 00:59:34 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -9,29 +9,37 @@ public class Main {
 
     public static int changeOs(String name, int year) {
         int currentYear = LocalDate.now().getYear();
-        if (year<currentYear){
+        if (year < currentYear) {
             System.out.println("обновите версию");
         } else {
             System.out.println("установите текущую версию");
         }
-        if (name.equals("IOS")){
+        if (name.equals("IOS")) {
             System.out.println("IOS");
             return 1;
         } else {
             System.out.println("ANDROID");
-        } return 0;
+        }
+        return 0;
 
 
+    }
 
+    public static int deliverToclient(int a, int b) {
+        if (a > 20) {
+            b++;
+        }
 
-
-
+        if (a > 60) {
+            b++;
+        }
+        return b;
     }
 
 
     public static void main(String[] args) {
         int year = 2020;
-        if (checkYear(year) == 0) System.out.println(" високосный ");
+        if (checkYear(year) == 0) System.out.println("високосный ");
         else System.out.println(" не високосный ");
         int specialYear = checkYear(year);
 
@@ -39,7 +47,13 @@ public class Main {
 
         String nameOs = "ANDROID";
         int osYear = 2022;
-        int clientOs = changeOs(nameOs,osYear);
+        int clientOs = changeOs(nameOs, osYear);
+
+        System.out.println(" задача 3 ");
+        int deliveryDistance = 80;
+        int deliveryDays = 1;
+        deliveryDays = deliverToclient(deliveryDistance,deliveryDays);
+        System.out.println("потребуется дней " + deliveryDays);
 
 
 
